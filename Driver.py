f78b47b2d863ba1a055088c5c599788f9d5c31bb
Xilin Liu <xal1@rice.edu>
2015-09-12 16:13:44 -0400

    errorvec

--- Driver.py
+++ Driver.py
@@ -2,15 +2,20 @@ from SonataNeuralNetwork import SonataNeuralNetwork, MIDIReader
 from music21.stream import Stream
 from os import listdir, path
 from math import log
+from numpy.random import normal
 
 
-music_dir = './tmidi/'
+music_dir = './midi/'
 
 
 def stream_from_notes(notes):
     for i in xrange(100):
-        inp = default + notes[-4] + notes[-3] + notes[-2] + notes[-1]
-        notes.append(list(net.activate(inp)))
+        inp = default + [j for i in notes for j in i][-12:]
+        new = list(net.activate(inp))
+        for i in new:
+            new[0] += normal(dur[0], dur[1])
+            new[1] += normal(freq[0], freq[1])
+        notes.append(new)
 
     return MIDIReader.list_to_stream(notes)
 
@@ -22,11 +27,12 @@ if __name__ == '__main__':
     print 'training'
     net = snn.train_network()
 
-    # b_net = bassNN.train_network()
+    snn.append_errors()
+    dur, freq = snn.get_error_vals()
 
     default = [4, 4]
-    treble = stream_from_notes([[1, log(440)], [1, log(500)], [1, log(550)], [1, log(500)]])
-    bass = stream_from_notes([[0.5, log(220)], [0.5, log(300)], [0.5, log(350)], [1, log(400)], [0.5, log(350)], [0.5, log(300)]])
+    treble = stream_from_notes([[1, log(440)], [1, log(500)], [1, log(550)], [1, log(500)], [1, log(550)], [1, log(500)]])
+    bass = stream_from_notes([[0.5, log(220)], [0.5, log(300)], [0.5, log(350)], [1, log(400)], [0.5, log(350)], [1, log(300)]])
     s = Stream()
     s.append(treble)
     s.append(bass)
--- SonataNeuralNetwork.py
+++ SonataNeuralNetwork.py
@@ -3,24 +3,26 @@ from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.tools.shortcuts import buildNetwork
 import music21 as m2
 from math import log, e
+from numpy import std, mean
+
 
 class MIDIReader(object):
 
-    @staticmethod
-    def parse(filename):
-        piece = {}
-        piece['score'] = m2.converter.parse(filename)
-        piece['key_sigs'] = [(sig.sharps, sig.getOffsetBySite(piece['score'][0]))
-                             for sig in list(piece['score'][0].getElementsByClass(m2.key.KeySignature))]
-        piece['time_sigs'] = [(sig.beat, sig.denominator, sig.getOffsetBySite(piece['score'][0]))
-                              for sig in list(piece['score'][0].getElementsByClass(m2.meter.TimeSignature))]
-        return piece
+    def __init__(self, filename):
+        self.piece = {}
+        self.piece['score'] = m2.converter.parse(filename)
+        self.piece['key_sigs'] = [(sig.sharps, sig.getOffsetBySite(self.piece['score'][0]))
+                                  for sig in list(self.piece['score'][0].getElementsByClass(m2.key.KeySignature))]
+        self.piece['time_sigs'] = [(sig.beat, sig.denominator, sig.getOffsetBySite(self.piece['score'][0]))
+                                   for sig in list(self.piece['score'][0].getElementsByClass(m2.meter.TimeSignature))]
 
-    @staticmethod
-    def get_notes(staff, key_sig):
+    def get_notes(self):
+        notes = []
         key_sig_adjust = {-7: 1, -6: 6, -5: -1, -4: 4, -3: -3, -2: 2, -1: -5, 0: 0, 1: 5, 2: -2, 3: 3, 4: -4, 5: 1, 6: -6, 7: -1}
-        return [MIDIReader.get_chord_or_note(note).transpose(key_sig_adjust[key_sig])
-                for note in staff if type(note) in [m2.note.Note, m2.chord.Chord]]
+        for staff in self.piece['score']:
+            notes.append([MIDIReader.get_chord_or_note(note).transpose(key_sig_adjust[self.piece['key_sigs'][0][0]])
+                          for note in staff if type(note) in [m2.note.Note, m2.chord.Chord]])
+        return notes
 
     @staticmethod
     def get_chord_or_note(chord_or_note):
@@ -38,22 +40,25 @@ class MIDIReader(object):
         for note in note_list:
             p = m2.pitch.Pitch()
             p.frequency = e ** note[1]
-            stream.append(m2.note.Note(p, duration=m2.duration.Duration(round(note[0]*256)/256.0)))
+            stream.append(m2.note.Note(p, duration=m2.duration.Duration(round(note[0]*64)/64.0)))
         return stream
 
 
 class SonataNeuralNetwork(object):
 
-    def __init__(self, prev=3):
+    def __init__(self, prev=5):
         self.ds = SupervisedDataSet(2 + (prev+1) * 2, 2)
-        self.net = buildNetwork(2 + (prev+1) * 2, 10, 15, 10, 2)
+        self.net = buildNetwork(2 + (prev+1) * 2, 50, 75, 25, 2)
         self.prev = prev
+        self.corpus = []
+        self.freq_err = []
+        self.dur_err = []
 
     def read(self, filename):
-        piece = MIDIReader.parse(filename)
-        for staff in piece['score']:
-            notes = MIDIReader.get_notes(staff, piece['key_sigs'][0][0])
-            self.append_dataset(notes, piece['time_sigs'][0])
+        piece = MIDIReader(filename)
+        self.corpus.append(piece)
+        for notes in piece.get_notes():
+            self.append_dataset(notes, piece.piece['time_sigs'][0])
 
     def append_dataset(self, notes, time_sig):
         for i in xrange(self.prev, len(notes) - 1):
@@ -68,3 +73,17 @@ class SonataNeuralNetwork(object):
             # print trainer.train()
             continue
         return self.net
+
+    def append_errors(self):
+        for piece in self.corpus:
+            for notes in piece.get_notes():
+                for i in xrange(self.prev, len(notes) - 1):
+                    inp = [piece.piece['time_sigs'][0][0], piece.piece['time_sigs'][0][1]]
+                    for j in xrange(self.prev, -1, -1):
+                        inp += [notes[i-j].duration.quarterLength, log(notes[i-j].frequency)]
+                    dur, freq = self.net.activate(inp)
+                    self.freq_err.append(freq - log(notes[i+1].frequency))
+                    self.dur_err.append(dur - notes[i+1].duration.quarterLength)
+
+    def get_error_vals(self):
+        return (mean(self.dur_err), std(self.dur_err)), (mean(self.freq_err), std(self.freq_err))
