c8643ebe30af5c7f3d983fac2503ae0f01529835
Xilin Liu <xal1@rice.edu>
2015-09-12 22:27:41 -0400

    chord progression and separate staves implemented, mutation by perfect chords

--- Driver.py
+++ Driver.py
@@ -1,20 +1,33 @@
 from SonataNeuralNetwork import SonataNeuralNetwork, MIDIReader
 from music21.stream import Stream
+from music21 import clef
 from os import listdir, path
 from math import log
-from numpy.random import normal
+from random import random
 
 
-music_dir = './midi/'
+music_dir = './newmidi/'
 
 
-def stream_from_notes(notes):
+def new_freq(old_freq, dist):
+    return 2 ** (dist / 12) * old_freq
+
+
+def stream_from_notes(notes, net, dur, freq):
     for i in xrange(100):
-        inp = default + [j for i in notes for j in i][-12:]
+        inp = default + [j for i in notes for j in i][-12:] + [notes[-3][1], notes[-2][1], notes[-1][1], notes[-1][1]]
         new = list(net.activate(inp))
-        for i in new:
-            new[0] += normal(dur[0], dur[1])
-            new[1] += normal(freq[0], freq[1])
+
+        r = random()
+        if r < 0.2:
+            new[1] = new_freq(new[1], 5)
+        elif r < 0.4:
+            new[1] = new_freq(new[1], 7)
+        elif r < 0.6:
+            new[1] = new_freq(new[1], 12)
+        # for i in new:
+        #     new[0] += normal(dur[0], dur[1])
+        #     new[1] += normal(freq[0], freq[1])
         notes.append(new)
 
     return MIDIReader.list_to_stream(notes)
@@ -25,14 +38,18 @@ if __name__ == '__main__':
         print midi
         snn.read(path.join(music_dir, midi))
     print 'training'
-    net = snn.train_network()
+    t_net, b_net = snn.train_network()
 
     snn.append_errors()
-    dur, freq = snn.get_error_vals()
+    td, tf, bd, bf = snn.get_error_vals()
 
     default = [4, 4]
-    treble = stream_from_notes([[1, log(440)], [1, log(500)], [1, log(550)], [1, log(500)], [1, log(550)], [1, log(500)]])
-    bass = stream_from_notes([[0.5, log(220)], [0.5, log(300)], [0.5, log(350)], [1, log(400)], [0.5, log(350)], [1, log(300)]])
+    treble = stream_from_notes([[1, log(440)], [1, log(500)], [1, log(550)], [1, log(500)], [1, log(550)], [1, log(500)]],
+                               t_net, td, tf)
+    treble.append(0, clef.TrebleClef())
+    bass = stream_from_notes([[0.5, log(220)], [0.5, log(300)], [0.5, log(350)], [1, log(400)], [0.5, log(350)], [1, log(300)]],
+                             b_net, bd, bf)
+    bass.insert(0, clef.BassClef())
     s = Stream()
     s.append(treble)
     s.append(bass)
--- SonataNeuralNetwork.py
+++ SonataNeuralNetwork.py
@@ -16,13 +16,33 @@ class MIDIReader(object):
         self.piece['time_sigs'] = [(sig.beat, sig.denominator, sig.getOffsetBySite(self.piece['score'][0]))
                                    for sig in list(self.piece['score'][0].getElementsByClass(m2.meter.TimeSignature))]
 
-    def get_notes(self):
-        notes = []
+    def process_notes(self):
+        self.piece['treble'] = []
+        self.piece['bass'] = []
+        self.piece['chords'] = {}
         key_sig_adjust = {-7: 1, -6: 6, -5: -1, -4: 4, -3: -3, -2: 2, -1: -5, 0: 0, 1: 5, 2: -2, 3: 3, 4: -4, 5: 1, 6: -6, 7: -1}
-        for staff in self.piece['score']:
-            notes.append([MIDIReader.get_chord_or_note(note).transpose(key_sig_adjust[self.piece['key_sigs'][0][0]])
-                          for note in staff if type(note) in [m2.note.Note, m2.chord.Chord]])
-        return notes
+        for note in self.piece['score'][0].flat:  # treble
+            if type(note) in [m2.note.Note, m2.chord.Chord]:
+                offset = note.getOffsetBySite(self.piece['score'][0].flat)
+                self.piece['treble'].append((MIDIReader.get_chord_or_note(note)
+                    .transpose(key_sig_adjust[self.piece['key_sigs'][0][0]]), offset))
+                if offset % 1 == 0:
+                    if offset in self.piece['chords']:
+                        self.piece['chords'][offset] = m2.chord.Chord([note, self.piece['chords'][offset]])
+                    else:
+                        self.piece['chords'][offset] = m2.chord.Chord([note])
+
+        for note in self.piece['score'][1].flat:  # bass
+            if type(note) in [m2.note.Note, m2.chord.Chord]:
+                offset = note.getOffsetBySite(self.piece['score'][1].flat)
+                self.piece['bass'].append((MIDIReader.get_chord_or_note(note)
+                    .transpose(key_sig_adjust[self.piece['key_sigs'][0][0]]), offset))
+                if offset in self.piece['chords']:
+                    self.piece['chords'][offset] = m2.chord.Chord([note, self.piece['chords'][offset]])
+                else:
+                    self.piece['chords'][offset] = m2.chord.Chord([note])
+
+        return self.piece['treble'], self.piece['bass'], self.piece['chords']
 
     @staticmethod
     def get_chord_or_note(chord_or_note):
@@ -40,50 +60,90 @@ class MIDIReader(object):
         for note in note_list:
             p = m2.pitch.Pitch()
             p.frequency = e ** note[1]
-            stream.append(m2.note.Note(p, duration=m2.duration.Duration(round(note[0]*64)/64.0)))
+            stream.append(m2.note.Note(p, duration=m2.duration.Duration(round(note[0]*4)/4.0)))
         return stream
 
 
 class SonataNeuralNetwork(object):
 
     def __init__(self, prev=5):
-        self.ds = SupervisedDataSet(2 + (prev+1) * 2, 2)
-        self.net = buildNetwork(2 + (prev+1) * 2, 50, 75, 25, 2)
+        # timsig beat, timsig denom, prev + curr dur/freq, prev 3 chords, bass note
+        self.t_ds = SupervisedDataSet(2 + (prev+1) * 2 + 4, 2)
+        self.t_net = buildNetwork(2 + (prev+1) * 2 + 4, 50, 75, 25, 2)
+        self.t_freq_err = []
+        self.t_dur_err = []
+
+        self.b_ds = SupervisedDataSet(2 + (prev+1) * 2 + 4, 2)
+        self.b_net = buildNetwork(2 + (prev+1) * 2 + 4, 50, 75, 25, 2)
+        self.b_freq_err = []
+        self.b_dur_err = []
+
         self.prev = prev
         self.corpus = []
-        self.freq_err = []
-        self.dur_err = []
 
     def read(self, filename):
         piece = MIDIReader(filename)
         self.corpus.append(piece)
-        for notes in piece.get_notes():
-            self.append_dataset(notes, piece.piece['time_sigs'][0])
+        treb, bass, chords = piece.process_notes()
+        self.append_dataset(treb, piece)
+        self.append_dataset(bass, piece, False)
 
-    def append_dataset(self, notes, time_sig):
-        for i in xrange(self.prev, len(notes) - 1):
-            inp = [time_sig[0], time_sig[1]]
-            for j in xrange(self.prev, -1, -1):
-                inp += [notes[i-j].duration.quarterLength, log(notes[i-j].frequency)]
-            self.ds.addSample(inp, (notes[i+1].duration.quarterLength, log(notes[i+1].frequency)))
+    def append_dataset(self, staff, piece, treble=True):
+        for i in xrange(self.prev, len(staff) - 1):
+            inp = self.get_input(staff, i, piece, treble)
+            ds = self.t_ds if treble else self.b_ds
+            ds.addSample(inp, (staff[i+1][0].duration.quarterLength, log(staff[i+1][0].frequency)))
 
     def train_network(self):
-        trainer = BackpropTrainer(self.net, self.ds)
-        while trainer.train() > 3800:
-            # print trainer.train()
+        t_trainer = BackpropTrainer(self.t_net, self.t_ds)
+        while t_trainer.train() > 3800:
+            print t_trainer.train()
             continue
-        return self.net
+        b_trainer = BackpropTrainer(self.b_net, self.b_ds)
+        while b_trainer.train() > 3800:
+            print b_trainer.train()
+            continue
+        return self.t_net, self.b_net
 
     def append_errors(self):
         for piece in self.corpus:
-            for notes in piece.get_notes():
-                for i in xrange(self.prev, len(notes) - 1):
-                    inp = [piece.piece['time_sigs'][0][0], piece.piece['time_sigs'][0][1]]
-                    for j in xrange(self.prev, -1, -1):
-                        inp += [notes[i-j].duration.quarterLength, log(notes[i-j].frequency)]
-                    dur, freq = self.net.activate(inp)
-                    self.freq_err.append(freq - log(notes[i+1].frequency))
-                    self.dur_err.append(dur - notes[i+1].duration.quarterLength)
+            staff = piece.piece['treble']
+            for i in xrange(self.prev, len(staff) - 1):
+                inp = self.get_input(staff, i, piece)
+                dur, freq = self.t_net.activate(inp)
+                self.t_freq_err.append(freq - log(staff[i+1][0].frequency))
+                self.t_dur_err.append(dur - staff[i+1][0].duration.quarterLength)
+
+            staff = piece.piece['bass']
+            for i in xrange(self.prev, len(staff) - 1):
+                inp = self.get_input(staff, i, piece)
+                dur, freq = self.b_net.activate(inp)
+                self.b_freq_err.append(freq - log(staff[i+1][0].frequency))
+                self.b_dur_err.append(dur - staff[i+1][0].duration.quarterLength)
+
+    def get_input(self, staff, i, piece, treble=True):
+        inp = list(piece.piece['time_sigs'][0][:2])
+        for j in xrange(self.prev, -1, -1):
+            inp += [staff[i-j][0].duration.quarterLength, log(staff[i-j][0].frequency)]
+
+        offset = int(staff[i][1])
+        log_freq = log(staff[i][0].frequency)
+        other_freq = piece.piece['score'][1 if treble else 0].getElementsByOffset(offset)
+        if len(other_freq) < 1:
+            other_freq = log_freq
+        elif type(other_freq[0]) == m2.note.Note:
+            other_freq = other_freq[0].frequency
+        elif type(other_freq[0]) == m2.chord.Chord:
+            other_freq = other_freq[0].root().frequency
+        else:
+            other_freq = log_freq
+
+        inp += [log(piece.piece['chords'][offset-2].root().frequency) if offset-2 in piece.piece['chords'] else log_freq,
+                log(piece.piece['chords'][offset-1].root().frequency) if offset-1 in piece.piece['chords'] else log_freq,
+                log(piece.piece['chords'][offset].root().frequency) if offset in piece.piece['chords'] else log_freq,
+                other_freq]
+        return inp
 
     def get_error_vals(self):
-        return (mean(self.dur_err), std(self.dur_err)), (mean(self.freq_err), std(self.freq_err))
+        return ((mean(self.t_dur_err), std(self.t_dur_err)), (mean(self.t_freq_err), std(self.t_freq_err)),
+                (mean(self.b_dur_err), std(self.b_dur_err)), (mean(self.b_freq_err), std(self.b_freq_err)))
